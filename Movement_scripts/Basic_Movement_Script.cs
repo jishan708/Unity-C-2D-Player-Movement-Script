19b61912bf13a71dbba3e2c14a8941cf473cfeba
jishan708 <jaofficial4759@gmail.com>
2026-06-03 19:33:40 +0530

    Implement basic player movement script

--- Movement_scripts/Basic_Movement_Script.cs
+++ Movement_scripts/Basic_Movement_Script.cs
@@ -1,1 +1,28 @@
 
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        // Input lena
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        // Movement
+        Vector3 movement = new Vector3(moveX, moveY, 0f);
+        transform.position += movement * speed * Time.deltaTime;
+
+        // Sprite Flip
+        if (moveX > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveX < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+}
